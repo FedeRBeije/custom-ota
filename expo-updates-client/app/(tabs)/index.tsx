0a4f7bdc1d6aaadb245298fed1d4d0bda8bd7942
Federico Reghini <federicoreghini@Federicos-Air.station>
2024-01-29 16:12:41 +0100

    wip

--- expo-updates-client/app/(tabs)/index.tsx
+++ expo-updates-client/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from "@/components/Themed";
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>super wow</Text>
+      <Text style={styles.title}>siamo contenti così</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
